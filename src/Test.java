e8cbd48349e5f865ced41f65f02ddd2a2875d2a8
apravai <apravai@EN61083.endava.net>
2013-07-22 10:10:40 +0300

    all implemented

--- src/Test.java
+++ src/Test.java
@@ -18,6 +18,7 @@ public class Test {
 		String line;
 		String[] studentInfo;
 		ArrayList<Student> students = new ArrayList<Student>();
+		
 		try {
 			in = new BufferedReader(new FileReader("Database"));
 		} catch (FileNotFoundException e) {
@@ -36,23 +37,32 @@ public class Test {
 			System.out.println("IOException");
 		}
 		// searching for a student...
-		Student s = studentFactory.createStudent("Graduate", "Pravai", "Andra",
+		Student myExampleStudent = studentFactory.createStudent("Graduate", "Pravai", "Andra",
 				123456, 1200);
 		for (Student st : students) {
-			if (st.equals(s)) {
+			if (st.equals(myExampleStudent)) {
 				System.out.println("Da!!");
 			}
 		}
 		// editing an existing student...
 		for (Student st : students) {
-			if (st.equals(s)) {
+			if (st.equals(myExampleStudent)) {
 				st.setFirstName("notPravai");
 			}
 		}
 		System.out.println(students.get(1));
 		// remove a student...
-		
+		myExampleStudent.setFirstName("notPravai");
+		for (Student st : students){
+			if (st.equals(myExampleStudent)){
+				students.remove(st);
+				break;
+			}
+		}
+		System.out.println(students.get(1));
 		// adding a student
+		students.add(1, myExampleStudent);
+		System.out.println(students.get(1));
 	}
 
 }
